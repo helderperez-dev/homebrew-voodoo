b48fcfc71c7bd6fdec9b86055070def5ccad88f1
Helder Perez <helder.perez@avynta.com>
2026-08-14 20:32:51 -0300

    Bump voodoo-framework to 1.0.6

--- Formula/voodoo.rb
+++ Formula/voodoo.rb
@@ -3,8 +3,8 @@ class Voodoo < Formula
 
   desc "Fast, Animated, AI-Powered Python Web Framework"
   homepage "https://github.com/helderperez-dev/voodoo"
-  url "https://files.pythonhosted.org/packages/source/v/voodoo-framework/voodoo_framework-1.0.4.tar.gz"
-  sha256 "327e688c7d447f69179d735dcd01c4089443af2bf68a83f18a092fd3b7fd02bd"
+  url "https://files.pythonhosted.org/packages/source/v/voodoo-framework/voodoo_framework-1.0.6.tar.gz"
+  sha256 "b2b21b230b76d246be937d88146415ab803109af3a2f242fc7bb08a9fc7d9cf7"
   license "MIT"
 
   depends_on "python@3.12"
