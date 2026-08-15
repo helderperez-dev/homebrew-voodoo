8d90c439cce5da22b2ce5efe5bd5ae113cfa5f4e
Helder Perez <contact@helderperez.com>
2026-08-15 17:09:37 +0000

    chore(release): update voodoo to 1.0.22

--- Formula/voodoo.rb
+++ Formula/voodoo.rb
@@ -3,8 +3,8 @@ class Voodoo < Formula
 
   desc "Fast, Animated, AI-Powered Python Web Framework"
   homepage "https://github.com/helderperez-dev/voodoo"
-  url "https://files.pythonhosted.org/packages/source/v/voodoo-framework/voodoo_framework-1.0.21.tar.gz"
-  sha256 "759a573f2f7e19e693ef495f2929d540e5f11847fd412b30f145ba6952a8b791"
+  url "https://files.pythonhosted.org/packages/source/v/voodoo-framework/voodoo_framework-1.0.22.tar.gz"
+  sha256 "022c5e48c2d013f6e301fa9f44799d73f5cfca0cbc2320409e3b50a891364c57"
   license "MIT"
 
   depends_on "python@3.12"
